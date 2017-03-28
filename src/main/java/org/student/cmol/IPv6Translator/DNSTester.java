bf3712703e4f9395a4a4311339b988424d07f5e6
Claus Lensbøl <cmol@cmol.dk>
2017-03-28 11:20:24 +0200

    Working prototype for reading dns

--- src/main/java/org/student/cmol/IPv6Translator/DNSTester.java
+++ src/main/java/org/student/cmol/IPv6Translator/DNSTester.java
@@ -9,11 +9,20 @@ public class DNSTester {
 
 
     DNSTester() {
-        byte in_pkt[] = { /* Packet 1 */
+        /*byte in_pkt[] = { /* Packet 1
                 (byte) 0xa8, 0x12, 0x01, 0x20, 0x00, 0x01, 0x00, 0x00,
                 0x00, 0x00, 0x00, 0x01, 0x04, 0x63, 0x6d, 0x6f,
                 0x6c, 0x02, 0x64, 0x6b, 0x00, 0x00, 0x01, 0x00,
                 0x01, 0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00 };*/
+
+        byte in_pkt[] = { /* Packet 4 */
+                (byte) 0xae, 0x1b, (byte) 0x81, (byte) 0x80, 0x00, 0x01, 0x00, 0x01,
+                0x00, 0x00, 0x00, 0x01, 0x04, 0x63, 0x6d, 0x6f,
+                0x6c, 0x02, 0x64, 0x6b, 0x00, 0x00, 0x01, 0x00,
+                0x01, (byte) 0xc0, 0x0c, 0x00, 0x01, 0x00, 0x01, 0x00,
+                0x00, 0x1b, 0x47, 0x00, 0x04, 0x5d, 0x5a, 0x72,
+                0x37, 0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00,
                 0x00, 0x00, 0x00, 0x00 };
 
 
@@ -38,42 +47,66 @@ public class DNSTester {
         //int bb_current = bb_start;
         //for(int i = dns_nQue; i > 0; i--) {
 
-        int len = (bb_in.get() & 0xff);
+        // Forget about trying to read the name, just skip the thing..
+        while(true) {
+            int len = (bb_in.get() & 0xff);
+            System.out.println(len);
+            if(len == 0) {break;}
+            else {
+                bb_in.position(bb_in.position() + len);
+            }
+        }
+
+        /*
         ByteBuffer bb_question = ByteBuffer.wrap(new byte[len]);
         byte byte_question[] = new byte[len];
         bb_in.get(byte_question);
         bb_question.put(byte_question);
         bb_question.flip();
         String qname = bb_question.toString();
-        System.out.println(qname);
+        System.out.println(qname);*/
         short qtype = bb_in.getShort();
         short qclass = bb_in.getShort();
 
+        System.out.println("t:"+qtype+" c:"+qclass);
+
 
         // Read the answer section
-        len = (bb_in.get(bb_in.position()) & 0xff);
-
-        String rr_name;
-        int new_pos;
-
+        System.out.println("pos:" + bb_in.position());
+        int len = (bb_in.get(bb_in.position()) & 0xff);
+        System.out.println("pos:" + bb_in.position());
+        System.out.println("Lal");
+        System.out.println(len);
         // Pointer to name another place
         if (len >= 192) {
             // Get position and adjust for the last two bits
-            new_pos = bb_in.position() + 2;
 
-            int pos = (bb_in.getShort() & 0x3fff);
+            bb_in.position(bb_in.position() + 2);
+
+            /*int pos = (bb_in.getShort() & 0x3fff);
             int nlen = (bb_in.get(pos) & 0xff);
             byte bb_rr_name[] = new byte[nlen];
             bb_in.get(bb_rr_name, pos + 1, nlen);
             rr_name = bb_rr_name.toString();
-            bb_in.position(new_pos);
+            bb_in.position(new_pos);*/
         }
         // Name is here
         else {
-            int nlen = (bb_in.get() & 0xff);
+            // Skip the damn name..
+            bb_in.position(bb_in.position() + len);
+            while(true) {
+                len = (bb_in.get() & 0xff);
+                System.out.println(len);
+                if(len == 0) {break;}
+                else {
+                    bb_in.position(bb_in.position() + len);
+                }
+            }
+
+            /*int nlen = (bb_in.get() & 0xff);
             byte bb_rr_name[] = new byte[nlen];
             bb_in.get(bb_rr_name);
-            rr_name = bb_rr_name.toString();
+            rr_name = bb_rr_name.toString();*/
         }
 
         short rr_type   = bb_in.getShort();
